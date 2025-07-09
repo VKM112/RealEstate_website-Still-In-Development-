ea373429f74180ee39853064ebd0c93fdc589abe
Mohan <vkmohan112@gmail.com>
2025-07-09 15:07:10 +0530

    Fourth commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
-import PropertyList from "./components/propertyList/PropertyList"
-import './App.css'
-import SearchBar from "./components/searchBar/searchBar"
-const App = () => {
-  return(
-    <div className="Main">
-      <h1>Welcome to realestate business</h1>
-      <SearchBar/>
-      <PropertyList />
-    </div>
-  )
-}
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import PropertyList from "./components/propertyList/PropertyList";
+import SearchBar from "./components/searchBar/SearchBar";
+import './App.css';
 
-export default App
+const App = () => {
+  return (
+    <BrowserRouter>
+      <div className="Main">
+        <h1>Welcome to real estate business</h1>
+        <SearchBar />
+        
+        <Routes>
+          <Route path="/" element={<PropertyList />} />
+          {/* Future route: details */}
+          {/* <Route path="/property/:id" element={<PropertyDetails />} /> */}
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+};
+
+export default App;
